b28280165fc424ae816fbeddb4b5d88aa090b4f9
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2019-10-18 12:23:50 +0530

    Use llist for managing memory

--- malloc.c
+++ malloc.c
@@ -10,22 +10,24 @@
 #include <unistd.h>
 
 #include "malloc.h"
+#include "llist.h"
 
 #define INCREMENT 4096  // generally 4KB is the default page size
 
 // global variables
-void* curr_brk, *blocks;
-void* (*mem_block_algo)(void*, int);  // function pointer to chosen algorithm
-void* (*failsafe_block_algo)(void*, int);  // failsafe algo is called after requesting memory
-void* (*defrag_block_algo)(void*);  // combine small blocks of memory together
+LL_HEAD(head);
+void* (*mem_block_algo)(int);  // function pointer to chosen algorithm
+void* (*failsafe_block_algo)(int);  // failsafe algo is called after requesting memory
+void (*defrag_block_algo)(void*);  // combine small blocks of memory together
 int ret;
 
 // block header structure
 typedef struct block_hdr {
+    struct ll_head* prev;
+    struct ll_head* next;
     int size;
     bool block_start;
     bool allocated;
-    struct block_hdr* next;
 } block_hdr;
 typedef block_hdr* Block_hdr;
 
@@ -39,42 +41,66 @@ typedef block_hdr* Block_hdr;
  * Traverse all blocks and return first unallocated
  * block that satisfies size requirement
  */
-void* first_fit_block_algo(void* blocks, int size) {
+void* first_fit_block_algo(int size) {
 
     // initialize variables to traverse blocks
-    void* traverse = blocks;
     void* alloc_block = NULL;
+    struct llhead* traverse;
     Block_hdr hdr;
     
-    while (true) {
-        if (traverse == NULL) return NULL;
-
+    LL_FOREACH(&head, traverse) {
         hdr = (Block_hdr) traverse;
+
+        // Note: experiment with this check to get different results
         // checks if block has size + bytes required for next block header
         if (!hdr->allocated && hdr->size >= size + sizeof(block_hdr)) {
-            alloc_block = traverse;
+            alloc_block = hdr;
             break;
         }
-
-        traverse = hdr->next;
     }
 
     return alloc_block;
 }
 
 /**
- * Checks if first block satisfied memory requirement
+ * Checks if last block satisfied memory requirement
  * Note: Ideally should be called after requesting more memory
+ * as newly created arena will be added to tail of blocks
  */
-void* failsafe_check_new_block(void* blocks, int size) {
+void* failsafe_check_tail_block(int size) {
 
-    void* alloc_block = NULL;
-    Block_hdr hdr = (Block_hdr) blocks;
+    struct llhead* tail = (&head)->prev;
+    Block_hdr hdr = (Block_hdr) tail;
+
+    // return NULL if tail block is same as head
+    if (tail == &head) return NULL;
+    // tail block has is unallocated has space and 
     if (!hdr->allocated && hdr->size >= size + sizeof(block_hdr)) {
-        alloc_block = blocks;
+        return hdr;
     }
 
-    return alloc_block;
+    return NULL;
+}
+
+/**
+ * Merge adjacent unallocated blocks
+ */
+void merge_adjacent_blocks_algo(void* ptr) {
+
+    struct llhead* next, *traverse, *block = (struct llhead*) ptr;
+    Block_hdr tmp, hdr = (Block_hdr) ptr;
+
+    // forward pass uptil allocated block or start of next block 
+    LL_FOREACH_SAFE(block, next, traverse) {
+
+        tmp = (Block_hdr) next;
+        if (tmp == ((Block_hdr) &head)) break;
+        if (tmp->allocated || tmp->block_start) break;
+        
+        hdr->size += tmp->size + sizeof(block_hdr);
+        LL_DEL(next);
+        printf("merged\n");
+    }
 }
 
 /**
@@ -83,17 +109,16 @@ void* failsafe_check_new_block(void* blocks, int size) {
 int request_memory() {
 
     // increment current program break location
-    curr_brk = sbrk(0);
+    void* curr_brk = sbrk(0);
     int ret = brk(curr_brk + INCREMENT);
     if (ret == -1) return -1;  // failed to find program break location
 
-    // allocate new block of memory
+    // allocate new arena of memory
     Block_hdr hdr = (Block_hdr) curr_brk;
     hdr->size = INCREMENT - sizeof(block_hdr);
-    hdr->next = blocks;
     hdr->allocated = false;
     hdr->block_start = true;
-    blocks = hdr;
+    LL_TAIL(&head, ((struct llhead*) hdr));
 
     return 0;  // successfully allocated memory
 }
@@ -115,7 +140,8 @@ void* malloc_mem(void* block, int size) {
     new_hdr->allocated = true;
     new_hdr->size = empty_size - size - sizeof(block_hdr);
     new_hdr->next = hdr->next;
-    hdr->size -= size;
+    hdr->size = size;
+    LL_ADD(&head, (struct llhead*) new_hdr);
 
     return empty_mem;
 }
@@ -133,16 +159,19 @@ void* malloc_mem(void* block, int size) {
  * if function is passed invalid function name returns with -1
  */
 int init_malloc(char* algo_name) {
-    // initialize global variables
-    blocks = NULL;
-    curr_brk = NULL;
 
     // set mem_block_algo memory allocation
-    if (strcmp(algo_name, "first_fit_block_algo")) {
-        mem_block_algo = &first_fit_block_algo;
+    if (strcmp(algo_name, "first_fit_block_algo") == 0) {
+        mem_block_algo = first_fit_block_algo;
     } else {
         return -1;
     }
+
+    failsafe_block_algo = failsafe_check_tail_block;
+    defrag_block_algo = merge_adjacent_blocks_algo;
+
+    // request new arena
+    request_memory();
 }
 
 /**
@@ -150,16 +179,16 @@ int init_malloc(char* algo_name) {
  * return pointer to memory if successful else
  * returns NULL
  */
-void* custom_malloc(int size) {
+void* tmalloc(int size) {
     
     void* alloc_block;
-    alloc_block = mem_block_algo(blocks, size);
+    alloc_block = (*mem_block_algo)(size);
 
     // malloc_mem fails, request more memory from program stack
     if (alloc_block == NULL) {
         ret = request_memory();
         if (ret == 0) {  // successful block allocation
-            alloc_block = failsafe_block_algo(blocks, size);
+            alloc_block = (*failsafe_block_algo)(size);
         }
     }
 
@@ -174,24 +203,23 @@ void* custom_malloc(int size) {
  * Frees given block
  * Note: fails silently
  */
-void custom_free(void* ptr) {
+void tfree(void* ptr) {
     
     Block_hdr ptr_hdr = (Block_hdr) (ptr - sizeof(block_hdr));
-    Block_hdr traverse = ptr_hdr->next;
     ptr_hdr->allocated = false;
-    
+    (*defrag_block_algo)(ptr_hdr);
 }
 
-int main() {
-    // printf("%d\n", getpid());
-    printf("%d\n", getpid()); 
-    init_malloc("first_fit_block_algo");
-    int count = 0;
-    while (true) {
-        ret = request_memory();
-        if (ret == -1) break;
-        count++;
-    }
-    printf("%d\n", count);
-    getchar();
-}
+// int main() {
+//     // printf("%d\n", getpid());
+//     printf("%d\n", getpid()); 
+//     init_malloc("first_fit_block_algo");
+//     int count = 0;
+//     while (true) {
+//         ret = request_memory();
+//         if (ret == -1) break;
+//         count++;
+//     }
+//     printf("%d\n", count);
+//     getchar();
+// }
--- malloc.h
+++ malloc.h
@@ -2,7 +2,7 @@
 #define MALLOC_H
 
 int init_malloc(char* mem_block_algo);
-void* custom_malloc(int size);
-void custom_free(void* ptr);
+void* tmalloc(int size);
+void tfree(void* ptr);
 
 #endif
